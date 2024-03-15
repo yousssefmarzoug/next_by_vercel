c8d1c70ed2faf23314e754470ae608730a2ac3df
youssef marzoug <youssefmarzoug19@gmail.com>
2024-03-15 17:04:59 +0100

    finil-update-2

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,19 +1,24 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
-import { updateInvoice } from '@/app/lib/actions';
-
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
- 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Edit Invoice',
+};
+
 export default async function Page({ params }: { params: { id: string } }) {
-          const id = params.id;
-          const [invoice, customers] = await Promise.all([
-                    fetchInvoiceById(id),
-                    fetchCustomers(),
-                  ]);
-                  if (!invoice) {
-                    notFound();
-                  }
+  const id = params.id;
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+
+  if (!invoice) {
+    notFound();
+  }
+
   return (
     <main>
       <Breadcrumbs
@@ -26,7 +31,7 @@ export default async function Page({ params }: { params: { id: string } }) {
           },
         ]}
       />
-      <Form invoice={invoice} customers={customers} />
+      <Form  customers={customers} />
     </main>
   );
 }
