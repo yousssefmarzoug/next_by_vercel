c4b4c0d259ffc90bcec86cbd418d45a1428b117e
youssef marzoug <youssefmarzoug19@gmail.com>
2024-03-15 16:45:49 +0100

    finil-update

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -11,7 +11,7 @@ import { Metadata } from 'next';
 export const metadata: Metadata = {
   title: 'Invoices',
 };
- 
+
 export default async function Page({
   searchParams,
 }: {
@@ -22,7 +22,9 @@ export default async function Page({
 }) {
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
+
   const totalPages = await fetchInvoicesPages(query);
+
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
@@ -35,11 +37,7 @@ export default async function Page({
       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
         <Table query={query} currentPage={currentPage} />
       </Suspense>
-      {/*  <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
-        <Table query={query} currentPage={currentPage} />
-      </Suspense> */}
       <div className="mt-5 flex w-full justify-center">
-        {/* <Pagination totalPages={totalPages} /> */}
         <Pagination totalPages={totalPages} />
       </div>
     </div>
