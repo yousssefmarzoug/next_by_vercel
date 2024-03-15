7450e7d45742fc3e3a6ca27b058b6e4bad0b7bc0
youssef marzoug <youssefmarzoug19@gmail.com>
2024-03-15 16:35:01 +0100

    finil

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,7 +1,9 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { updateInvoice } from '@/app/lib/actions';
 
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
  
 export default async function Page({ params }: { params: { id: string } }) {
           const id = params.id;
@@ -9,6 +11,9 @@ export default async function Page({ params }: { params: { id: string } }) {
                     fetchInvoiceById(id),
                     fetchCustomers(),
                   ]);
+                  if (!invoice) {
+                    notFound();
+                  }
   return (
     <main>
       <Breadcrumbs
